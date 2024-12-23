927c58830a8c56a33c67527b983c9dbda1e7d22a
jhiagho <jhiagho@gmail.com>
2024-12-23 07:55:31 -0300

    adcionado o controle de fluxo

--- ControleFluxo/src/Contador.java
+++ ControleFluxo/src/Contador.java
@@ -3,17 +3,41 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
  */
 
+import java.util.Scanner;
+
 /**
  *
  * @author jhiag
  */
 public class Contador {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        // TODO code application logic here
-        System.err.println("Teste");
+        try {
+            Scanner terminal = new Scanner(System.in);
+            System.out.println("Digite o primeiro parâmetro");
+            int parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parâmetro");
+            int parametroDois = terminal.nextInt();
+
+            Contar(parametroUm,parametroDois);
+
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+    }
+
+    static void Contar(int parametroUm, int paramentroDois) throws ParametrosInvalidosException {
+        //Validar se paramentroUm e maior que paramentroDois e lançar a exceed.
+        if (parametroUm > paramentroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contador = 0;
+        for (int i = parametroUm; i <= paramentroDois; i++) {
+            System.out.println("Imprimindo o número: " + i);
+        }
+
     }
 }
